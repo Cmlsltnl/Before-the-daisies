14e6784b572f1a49bdef55dd506f407561fffdca
Sabina Shrestha <sabina.pari@yahoo.com>
2015-06-29 15:14:47 -0600

    changed the size of the paperclip for thumb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   # paperclip for the bucket list items
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "150x150#"}
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "50x50#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
