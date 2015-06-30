0c2b73d2416e126712e022f9221bd59a53910680
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-06-30 11:48:55 -0600

    added some twitter stuff, does not do much yet though

--- Gemfile
+++ Gemfile
@@ -23,6 +23,12 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'twitter'
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-facebook'
+gem 'omniauth-twitter', :github => 'arunagw/omniauth-twitter'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
 
-
   def index
+
+    config = {
+      consumer_key: ENV['twitter_key'],
+      consumer_secret: ENV['twitter_secret']
+    }
+    client = Twitter::REST::Client.new(config)
+    #@tweets = client.get_all_tweets("thebradhimself")
     @show_stuff = current_user.items.all
     @friends_stuff = current_user.friends
   end
@@ -9,6 +15,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
   end
+
 
 
 end
