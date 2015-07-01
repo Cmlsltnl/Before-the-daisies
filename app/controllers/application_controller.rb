989bc5d950a855bad3b87d4cab72f80d9f6710e1
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-07-01 16:27:21 -0600

    account update fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:sign_up) << :age
-    devise_parameter_sanitizer.for(:sign_up) << :city
-    devise_parameter_sanitizer.for(:sign_up) << :state
-    devise_parameter_sanitizer.for(:sign_up) << :zipcode
-    devise_parameter_sanitizer.for(:sign_up) << :image
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :age, :city, :state, :zipcode, :image, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :age, :city, :state, :zipcode, :image, :password, :password_confirmation, :current_password,) }
   end
 
   def after_sign_in_path_for(resource)
